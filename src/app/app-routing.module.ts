74f74fdb00e2e770528cc7cd68dcc6d7a62c6d95
Potato <poltoradnevone@gmail.com>
2022-11-05 07:08:35 +0400

    angular router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RoomListComponent } from './room-list/room-list.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+
+const routes: Routes = [
+  {
+    path: 'rooms',
+    component: RoomListComponent,
+  },
+  {
+    path: 'page',
+    component: SecondPageComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'rooms',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { RoomListComponent } from './room-list/room-list.component';
 import { ContainerComponent } from './container/container.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -14,8 +23,10 @@ import { ContainerComponent } from './container/container.component';
     RoomsComponent,
     RoomListComponent,
     ContainerComponent,
+    SecondPageComponent,
+    NavComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
